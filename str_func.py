8c8210bbdc8abda00d5d2549ed23a1e429050258
Dinara Uzbekova <dinarauzbekova7019@gmail.com>
2024-03-22 00:15:33 +0300

    funcs

--- str_func.py
+++ str_func.py
@@ -3,3 +3,10 @@ def upper_letters():
     """
     words = str(input())
     return words.upper()
+
+
+def capitalize_first_letters():
+    """The function receives words and capitalizes the first letters in them
+    """
+    words = str(input())
+    return words.title()
