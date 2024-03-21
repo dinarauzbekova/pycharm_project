cfbacd0c6d442903d0c99e92cfb57d5a2841f3a7
Dinara Uzbekova <dinarauzbekova7019@gmail.com>
2024-03-22 01:06:33 +0300

    funcs

--- str_func.py
+++ str_func.py
@@ -1,5 +1,13 @@
 def upper_letters():
     """The function gets words and capitalizes all the letters in them
+    because I want it
     """
     words = str(input())
     return words.upper()
+
+
+def capitalize():
+    """The function receives words and capitalizes the first letters in them
+    """
+    words = str(input())
+    return words.title()
